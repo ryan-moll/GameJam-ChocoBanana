26a6b9a2728a131343fb6844285c31a1b7b27154
ryan-moll <rmoll16@gmail.com>
2019-10-12 13:16:00 -0700

    Fixed initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Collision coll;
+    [HideInInspector]
+    public Rigidbody2D rb;
+
+    [Space]
+    [Header("Stats")]
+    public float speed = 10;
+    public float jumpForce = 50;
+
+    [Space]
+    [Header("Booleans")]
+    public bool canMove;
+
+    [Space]
+    private bool groundTouch;
+
+    public int side = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coll = GetComponent<Collision>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 dir = new Vector2(x, y);
+
+        Walk(dir);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+
+            if (coll.onGround)
+                Jump(Vector2.up);
+        }
+
+        if (coll.onGround && !groundTouch)
+        {
+            GroundTouch();
+            groundTouch = true;
+        }
+
+        if (!coll.onGround && groundTouch)
+        {
+            groundTouch = false;
+        }
+    }
+
+    private void Walk(Vector2 dir)
+    {
+        if (!canMove)
+            return;
+
+        rb.velocity = new Vector2(dir.x * speed, rb.velocity.y);
+    }
+
+    private void Jump(Vector2 dir)
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.velocity += dir * jumpForce;
+    }
+
+    void GroundTouch()
+    {
+    }
+}
